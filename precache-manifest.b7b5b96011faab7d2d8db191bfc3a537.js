b0b93d3dbd5b93a838bc371062884672bae08ea6
yana148 <yana148@gmail.com>
2019-07-21 13:44:34 +0300

    Updates

---
+++ precache-manifest.b7b5b96011faab7d2d8db191bfc3a537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a86693f5c1e7d7730f43612d6fa7b2",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "735b8b6fe555587fc815",
+    "url": "/giphy/static/css/main.3ec818a5.chunk.css"
+  },
+  {
+    "revision": "adbdd5ce58901ebae55a",
+    "url": "/giphy/static/js/2.50969d58.chunk.js"
+  },
+  {
+    "revision": "735b8b6fe555587fc815",
+    "url": "/giphy/static/js/main.1fbee2a1.chunk.js"
+  },
+  {
+    "revision": "2be08ba7eaf0823a821e",
+    "url": "/giphy/static/js/runtime~main.f83d3e2c.js"
+  }
+]);
